f0a1713e7244907dabeba5f1d8d9b6ed63c5cfca
Roei Erez <roeier@ac.sce.ac.il>
2017-11-25 08:28:10 +0200

    account controller

---
+++ ShookOnline/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using Facebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private Uri RedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
+        public ActionResult Facebook()
+        {
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = "1109182109184976",
+                client_secret = "76ff0efbb8c8953a9089615dc26dd28d",
+                redirect_uri = RedirectUri.AbsoluteUri,
+                response_type = "code",
+                scope = "email" // Add other permissions as needed
+            });
+
+            return Redirect(loginUrl.AbsoluteUri);
+        }
+        public ActionResult FacebookCallback(string code)
+        {
+            var fb = new FacebookClient();
+            dynamic result = fb.Post("oauth/access_token", new
+            {
+                client_id = "1109182109184976",
+                client_secret = "76ff0efbb8c8953a9089615dc26dd28d",
+                redirect_uri = RedirectUri.AbsoluteUri,
+                code = code
+            });
+
+            var accessToken = result.access_token;
+
+            // Store the access token in the session
+            Session["AccessToken"] = accessToken;
+
+            // update the facebook client with the access token so
+            // we can make requests on behalf of the user
+            fb.AccessToken = accessToken;
+
+            // Get the user's information
+            dynamic me = fb.Get("me?fields=first_name,last_name,id,email");
+            string email = me.email;
+            string firstName = me.first_name;
+            string lastName = me.last_name;
+            string id = me.id;
+            // Set the auth cookie
+            FormsAuthentication.SetAuthCookie(email, false);
+
+            return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
