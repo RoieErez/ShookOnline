6b7029a9067d71289b3e0c4818492ee4ab0ed136
Roei Erez <roeier@ac.sce.ac.il>
2017-12-18 17:21:52 +0200

    image upload

--- ShookOnline/Controllers/AccountController.cs
+++ ShookOnline/Controllers/AccountController.cs
@@ -4,7 +4,9 @@ using ShookOnline.Models;
 using System;
 using System.Data.Entity;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MVCApplication1.Controllers
@@ -127,6 +129,29 @@ namespace MVCApplication1.Controllers
         {
             return View();
         }
-       
+
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/images/profile"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+            }
+            // after successfully uploading redirect the user
+            return RedirectToAction("actionname", "controller name");
+        }
     }
 }
